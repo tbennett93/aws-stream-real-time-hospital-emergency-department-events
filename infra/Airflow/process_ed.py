1b256f4b6274709dafa58f97c255257f1476a133
tbennett93 <tbennett@hotmail.co.uk>
2026-02-26 20:45:03 +0000

    orchestrated parquet to redshift via glue in airflow

--- infra/Airflow/process_ed.py
+++ infra/Airflow/process_ed.py
@@ -1,14 +1,22 @@
 from airflow import DAG
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
+from datetime import timedelta
 
-from datetime import datetime, date
+from datetime import datetime
 
 with DAG(
     dag_id="process_ed",
     start_date=datetime(2024,1,1),
-    schedule=None,
+    schedule=timedelta(minutes=15) ,
     catchup=False,
+    default_args={
+        "retries": 2,
+        "aws_conn_id":"aws_default",
+        "wait_for_completion":True,
+        "region_name":"eu-west-2"        
+    },
+
 ):
 
     wait_for_file = S3KeySensor(
@@ -21,16 +29,21 @@ with DAG(
         timeout=600,
     )
 
-    run_glue = GlueJobOperator(
+    convert_to_parquet_glue = GlueJobOperator(
         task_id="convert_to_parquet",
-        job_name="ed-streaming-data-convert-json-to-parquet",   # already created in AWS
-        aws_conn_id="aws_default",
-        wait_for_completion=True,
-        region_name="eu-west-2"
+        job_name="ed-streaming-data-convert-json-to-parquet"   # already created in AWS
+  
+    )
+
+    copy_parquet_to_redshift_glue = GlueJobOperator(
+        task_id="copy_parquet_to_redshift",
+        job_name="ed-streaming-move-parquet-to-redshift"   # already created in AWS
+
     )
 
 
-    wait_for_file >> run_glue
+
+    wait_for_file >> convert_to_parquet_glue >> copy_parquet_to_redshift_glue
 
 
 #wait for file
