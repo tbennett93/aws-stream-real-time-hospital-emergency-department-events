08781440d994da67d5ac3a88fffee66967946dfd
Tom Bennett <tbennett@hotmail.co.uk>
2026-03-25 20:06:06 +0000

    Containerised the app. Airflow and dbt stored as images and containerised to integrate with each other

--- airflow/dags/process_ed.py
+++ airflow/dags/process_ed.py
@@ -1,9 +1,14 @@
 from airflow import DAG
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
-from datetime import timedelta
+from airflow.providers.docker.operators.docker import DockerOperator
 
+from docker.types import Mount
+
+
+from datetime import timedelta
 from datetime import datetime
+from airflow.hooks.base import BaseHook
 
 with DAG(
     dag_id="process_ed",
@@ -43,7 +48,34 @@ with DAG(
 
 
 
-    wait_for_file >> convert_to_parquet_glue >> copy_parquet_to_redshift_glue
+    aws_conn = BaseHook.get_connection("aws_default")
+
+
+
+
+    dbt_run = DockerOperator(
+        task_id="dbt_build",
+        image="your-dbt:latest",
+        command="build",
+        working_dir="/app",
+        auto_remove=True,
+        docker_url="unix://var/run/docker.sock",
+        mount_tmp_dir=False,
+        mounts=[
+            Mount(
+                source="C:\\Users\\tbenn\\.dbt",
+                target="/root/.dbt",
+                type="bind"
+            )
+        ],
+        environment={
+            "AWS_ACCESS_KEY_ID": aws_conn.login,
+            "AWS_SECRET_ACCESS_KEY": aws_conn.password,
+            "AWS_DEFAULT_REGION": "eu-west-2",
+        }
+    )
+
+    wait_for_file >> convert_to_parquet_glue >> copy_parquet_to_redshift_glue >> dbt_run
 
 
 #wait for file
